fb7bf0aa4033108c13f6623f82aa11caa1758051
LyNx725 <162893535+LyNx725@users.noreply.github.com>
2024-07-07 13:45:46 +0530

    Add files via upload

---
+++ dashboard.jsx
@@ -0,0 +1,15 @@
+
+import React, { useEffect, useState } from 'react';
+
+function Dashboard() {
+ 
+  return (
+    <div>
+      <h1>Dashboard</h1>
+  <img src="image.jpg"></img>
+      </div>
+    
+  );
+}
+
+export default Dashboard;
