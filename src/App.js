9a02460315fcdda70474ac7df4dd92a5ec62d93f
LyNx725 <162893535+LyNx725@users.noreply.github.com>
2024-07-07 13:59:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/navbar';
+import Dashboard from './components/dashboard';
+import AddProduct from './components/addproduct';
+import Footer from './components/footer';
+function App() {
+  return (
+    <>
+   
+<NavBar/>
+ <Dashboard/>
+ <AddProduct/>
+ <Footer/>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/dashboard.jsx
@@ -0,0 +1,17 @@
+
+import React, { useEffect, useState } from 'react';
+
+function Dashboard() {
+ 
+  return (
+  <div>
+    <h1>Dashboard</h1>
+
+    <div>
+  <img src="image.jpg"></img>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
